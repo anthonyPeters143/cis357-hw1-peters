1940c2734c1c1f5f43ed0351c73db36d9e7fe4de
anthonyPeters143 <108294207+anthonyPeters143@users.noreply.github.com>
2022-07-02 12:54:32 -0400

    Create CashRegister.java

---
+++ CashRegister.java
@@ -0,0 +1,22 @@
+public class CashRegister {
+    public static void main(String[] args) {
+
+        int testingInt = 1;
+
+
+        //import and initilaize ItemArray
+            //split by "," into ItemArray 0-29
+        
+        //prompt user Y/N for sale
+            //Y
+               //create NEW Sale object
+               //prompt for itemCode
+                  //if code == 1-10
+                      //use code to find item
+                  //if code == -1
+                      //print list/receipt and return to prompt user Y/N for sale
+                  //else retry code till valid
+            //N
+                //print daySaleTotal
+    }
+}
